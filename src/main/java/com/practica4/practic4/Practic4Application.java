170b795b094284df541139e1255549167f1cbfde
Jesus <jesus.pio@uabc.edu.mx>
2019-03-11 20:46:47 -0700

    Orale qlos

---
+++ src/main/java/com/practica4/practic4/Practic4Application.java
@@ -0,0 +1,13 @@
+package com.practica4.practic4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practic4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practic4Application.class, args);
+    }
+
+}
